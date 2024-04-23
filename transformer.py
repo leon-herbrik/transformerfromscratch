0a7a827836b9e3830b2ddfe842ebc158105d944a
wndlbltt <leon@herbrik.de>
2024-04-23 11:09:31 +0200

    Add attention calculation to Attention Layers.

--- transformer.py
+++ transformer.py
@@ -1,4 +1,4 @@
-from typing import List, Optional, Dict, Union
+from typing import List, Optional, Dict, Union, Any
 
 import torch
 from torch import Tensor
@@ -11,6 +11,7 @@ from torch.nn import (
     Linear,
     ReLU,
     LayerNorm,
+    Softmax,
 )
 import yaml
 import dotsi
@@ -36,10 +37,10 @@ class Transformer(Module):
             dotsi.Dict(self.default_config()) if config is None else dotsi.Dict(config)
         )
         config = self.config
-        config.n_heads, rest = divmod(config.dim, config.n_heads)
+        config.head_dim, rest = divmod(config.embed_dim, config.n_heads)
         if rest != 0:
             raise RuntimeError(
-                f"The embedding dimension needs to be divisible by the number of heads, however: {config.dim=} % {config.n_heads=} == {rest}"
+                f"The embedding dimension needs to be divisible by the number of heads, however: {config.embed_dim=} % {config.n_heads=} == {rest}"
             )
 
         self.tokenizer: Tokenizer = (
@@ -49,7 +50,7 @@ class Transformer(Module):
         )
         # We need an embedding layer that learns useful representations of each token.
         self.token_embedding: Embedding = Embedding(
-            num_embeddings=self.tokenizer.num_tokens, embedding_dim=config.dim
+            num_embeddings=self.tokenizer.num_tokens, embedding_dim=config.embed_dim
         )
         # Additionally, we need a positional embedding that makes it so the transformer isn't invariant to position.
         # This means, changing the embedding of a word based on its position in the sequence.
@@ -58,9 +59,9 @@ class Transformer(Module):
         #   - or a trainable one.
         match config.positional_embedding:
             case "ml" | "standard":
-                self.positional_embedding = self.Sinusoidal_Embedding(config)
+                self.positional_embedding = Sinusoidal_Embedding(config)
             case "learned":
-                self.positional_embedding = self.Learned_Embedding(config)
+                self.positional_embedding = Learned_Embedding(config)
             case _:
                 raise NotImplementedError(
                     f'Positional embedding: {config.positional_embedding} is not implemented. Try "ml", "standard", or "learned".'
@@ -105,19 +106,25 @@ class Transformer(Module):
                 # TODO: Add layer norm.
                 super().__init__()
                 self.config = config
-                self.attention = self.AttentionLayer(config)
+                self.attention = AttentionLayer(config)
                 self.feed_forward = Sequential(
-                    Linear(config.dim, config.dim),
+                    Linear(config.embed_dim, config.embed_dim),
                     ReLU(),
-                    Linear(config.dim, config.dim),
+                    Linear(config.embed_dim, config.embed_dim),
                 )
                 self.layer_norms = ModuleList(
                     [
                         LayerNorm(
-                            normalized_shape=(config.seq_len, config.dim)  # type:ignore
+                            normalized_shape=(
+                                config.seq_len,
+                                config.embed_dim,
+                            )  # type:ignore
                         ),
                         LayerNorm(
-                            normalized_shape=(config.seq_len, config.dim)  # type:ignore
+                            normalized_shape=(
+                                config.seq_len,
+                                config.embed_dim,
+                            )  # type:ignore
                         ),
                     ]
                 )
@@ -137,83 +144,13 @@ class Transformer(Module):
 
                 return x
 
-            class AttentionLayer(Module):
-                """
-                Attention layer for use in Encoder stack.
-                """
-
-                def __init(self, config):
-                    super().__init__()
-                    self.config = config
-
-                def forward(self, x):
-                    # TODO: Implement.
-                    return x
-
     class Decoder(Module):
         """
         Decoder of the transformer.
         """
 
-        def __init__(self, transformer):
-            super().__init__()
-
-    class Learned_Embedding(Module):
-        """
-        This is a learned positional embedding with trainable parameters. Empirically there should be no difference
-        between this and the sinusoidal one.
-        """
-
         def __init__(self, config):
             super().__init__()
-            self.embeddings = Embedding(
-                num_embeddings=config.seq_len, embedding_dim=config.dim
-            )
-
-        def forward(self, x):
-            num_embeddings = self.embeddings.num_embeddings
-            embedded = self.embeddings(torch.arange(num_embeddings))
-            return x + embedded
-
-    class Sinusoidal_Embedding(Module):
-        """
-        Using a slightly different variant of the embedding, because my friend Mahan and I found it more beautiful.
-        For PE(pos, 2i+1), we use cos(pos/10000^((2i+1)/d_model)) instead of cos(pos/10000^(2i/d_model)).
-        We find the matching (2i+1) nicer.
-        """
-
-        # TODO: Test difference between standard version and ML (Mahan & Leon) version.
-        def __init__(self, config):
-            super().__init__()
-            # 10000 is the proposed constant from the paper.
-            s, d, n = config.seq_len, config.dim, config.sinusoidal_embedding_constant
-            # Create tensor to hold the positional encodings for each position in sequence and dimension.
-            # This is a matrix holding values that can be added to the actual input data embeddings.
-            self.embeddings = torch.zeros(s, d)
-
-            positions = torch.arange(0, s).unsqueeze(1)
-
-            # Select the correct version.
-            match config.version:
-                case "ml":
-                    # For our own, very beautiful method, we need different denominators for the even and
-                    # positions in the embedding dimension.
-                    denominators_even = torch.pow(n, 2 * torch.arange(0, d, 2) / d)
-                    demoninators_odd = torch.pow(n, 2 * torch.arange(1, d, 2) / d)
-
-                    # Assign the actual sin and cos values to the positions in the embedding tensor.
-                    self.embeddings[:, 0::2] = torch.sin(positions / denominators_even)
-                    self.embeddings[:, 1::2] = torch.cos(positions / demoninators_odd)
-                case "standard":
-                    # Standard implementation.
-                    denominators = torch.pow(n, 2 * torch.arange(0, d // 2) / d)
-
-                    # Assign actual values as above.
-                    self.embeddings[:, 0::2] = torch.sin(positions / denominators)
-                    self.embeddings[:, 1::2] = torch.cos(positions / denominators)
-
-        def forward(self, x):
-            return x + self.embeddings
 
     def default_corpus(self) -> List[str]:
         """
@@ -236,13 +173,118 @@ class Transformer(Module):
         return yaml.safe_load(
             """
                 seq_len: 1024,
-                dim: 768,
+                embed_dim: 768,
                 n_heads: 8,
                 depth: 8,
                 positional_embedding: 'ml'
                 sinusoidal_embedding_constant: 10000
+                attention_bias: True
             """
         )
+
+
+class AttentionLayer(Module):
+    """
+    Attention layer.
+    """
+
+    def __init(self, config):
+        super().__init__()
+        self.config = config
+        # Linear layers.
+        self.layers: Dict = {}
+        for l in ["q", "k", "v"]:
+            self.layers[l] = ModuleList(
+                [
+                    Linear(
+                        config.embed_dim,
+                        config.head_dim,
+                        bias=config.attention_bias,
+                    )
+                    for _ in config.n_heads
+                ]
+            )
+            # Three linear layers for q, k and v each.
+        # Output layer o takes the concatenated outputs, which are then in the original embedding dimension again.
+        self.layers["o"] = Linear(
+            config.embed_dim, config.embed_dim, bias=config.attention_bias
+        )
+
+    def forward(self, x):
+        # Map input to each attention head.
+        queries = [layer(x) for layer in self.layers["q"]]
+        keys = [layer(x) for layer in self.layers["k"]]
+        values = [layer(x) for layer in self.layers["v"]]
+
+        return x
+
+    def attend(self, q: Tensor, k: Tensor, v: Tensor):
+        scale = torch.sqrt(self.config.embed_dim)
+        similarity = ((q @ k.t) / scale).softmax(dim=-1)
+        return v * similarity
+
+
+class Learned_Embedding(Module):
+    """
+    This is a learned positional embedding with trainable parameters. Empirically there should be no difference
+    between this and the sinusoidal one.
+    """
+
+    def __init__(self, config):
+        super().__init__()
+        self.embeddings = Embedding(
+            num_embeddings=config.seq_len, embedding_dim=config.embed_dim
+        )
+
+    def forward(self, x):
+        num_embeddings = self.embeddings.num_embeddings
+        embedded = self.embeddings(torch.arange(num_embeddings))
+        return x + embedded
+
+
+class Sinusoidal_Embedding(Module):
+    """
+    Using a slightly different variant of the embedding, because my friend Mahan and I found it more beautiful.
+    For PE(pos, 2i+1), we use cos(pos/10000^((2i+1)/d_model)) instead of cos(pos/10000^(2i/d_model)).
+    We find the matching (2i+1) nicer.
+    """
+
+    # TODO: Test difference between standard version and ML (Mahan & Leon) version.
+    def __init__(self, config):
+        super().__init__()
+        # 10000 is the proposed constant from the paper.
+        s, d, n = (
+            config.seq_len,
+            config.embed_dim,
+            config.sinusoidal_embedding_constant,
+        )
+        # Create tensor to hold the positional encodings for each position in sequence and dimension.
+        # This is a matrix holding values that can be added to the actual input data embeddings.
+        self.embeddings = torch.zeros(s, d)
+
+        positions = torch.arange(0, s).unsqueeze(1)
+
+        # Select the correct version.
+        match config.version:
+            case "ml":
+                # For our own, very beautiful method, we need different denominators for the even and
+                # positions in the embedding dimension.
+                denominators_even = torch.pow(n, 2 * torch.arange(0, d, 2) / d)
+                demoninators_odd = torch.pow(n, 2 * torch.arange(1, d, 2) / d)
+
+                # Assign the actual sin and cos values to the positions in the embedding tensor.
+                self.embeddings[:, 0::2] = torch.sin(positions / denominators_even)
+                self.embeddings[:, 1::2] = torch.cos(positions / demoninators_odd)
+            case "standard":
+                # Standard implementation.
+                denominators = torch.pow(n, 2 * torch.arange(0, d // 2) / d)
+
+                # Assign actual values as above.
+                self.embeddings[:, 0::2] = torch.sin(positions / denominators)
+                self.embeddings[:, 1::2] = torch.cos(positions / denominators)
+
+    def forward(self, x):
+        return x + self.embeddings
 
 
 def test():
